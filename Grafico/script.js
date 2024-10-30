1997221da9af6643ecba11e3702f64b54071072a
KauaPittnerMuniz <kaua.pittner.muniz@escola.pr.gov.br>
2024-10-30 11:04:09 -0300

    Add files via upload

--- Grafico/script.js
+++ Grafico/script.js
@@ -1,45 +1,46 @@
-const grafico = document.getElementById(meuGrafico).getContext("2d");
-const meuGrafico = new chart (grafico{
-type: "pie"
-data:{
-labels:[
-    "Instagram"
-    "WhatsApp"
-    "Facebook"
-    "WeChat"
-    "TikTok"
-    "DouYin"
-    "Twitter"
-    "Telegram"
-    "FB Messenger"
-    "LINE"
-]
-datasets:[{
-    data:[
-        16.5,
-        16.1,
-        12.8,
-        12.8,
-        7.4,
-        6.6,
-        3.2,
-        2.4,
-        2.3,
-        1.7,
-    ],
-    backgrouncolor:[
-        (75,0,130)
-        (128,0,0)
-        (255,0,0)
-        (255,255,0)
-        (255,0,255)
-        (0,100,0)
-        (0,255,255)
-        (0,0,205)
-        (211,211,211)
-        (186,85,211)
+const grafico = document.getElementById("meuGrafico").getContext("2d");
+const meuGrafico = new Chart(grafico, {
+    type: "pie",
+    data: {
+      labels: [
+        "Instagram", 
+        "WhatsApp", 
+        "Facebook", 
+        "WeChat", 
+        "TikTok", 
+        "DouYin", 
+        "Twitter", 
+        "Telegram", 
+        "FB Messenger", 
+        "LINE"
+      ],
+      datasets: [{
+        data: [
+          16.5, 
+          16.1, 
+          12.8, 
+          12.8, 
+          7.4, 
+          6.6, 
+          3.2, 
+          2.4, 
+          2.3, 
+          1.7
+        ],
+        backgroundColor: [
+          "red", 
+          "blue",
+          "green",
+          "#ADFF2F",
+          "#DAA520",
+          "#BC8F8F",
+          "rgb(245,222,179)",
+          "rgb(186,85,211)",
+          "rgb(255,20,147)",
+          "CadetBlue",        
+        ],
+      }
     ]
-}]
-}
-}
-)
+    }
+  }
+);
